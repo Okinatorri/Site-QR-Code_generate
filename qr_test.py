cd543eb9cc5a938b79fefc90562156321c398252
Yaroslav <zymer464@gmail.com>
2025-09-14 14:12:39 +0300

    Update qr_test.py

--- qr_test.py
+++ qr_test.py
@@ -10,16 +10,16 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
 def qr_genirating(url, foreground_color, background_color):
-    # --- Удаляем старые файлы старше 1 часа ---
+   
     for f in glob.glob(os.path.join(OUTPUT_DIR, "qr_*.png")):
-        if os.path.getmtime(f) < time.time() - 360:  # 3600 секунд = 1 час
+        if os.path.getmtime(f) < time.time() - 360:  
             os.remove(f)
 
-    # --- Генерируем уникальное имя файла ---
+    
     filename = f"qr_{uuid.uuid4().hex}.png"
     filepath = os.path.join(OUTPUT_DIR, filename)
 
-    # --- Создаём QR ---
+    
     qr = qrcode.QRCode(
         version=5,
         box_size=40,
@@ -35,6 +35,7 @@ def qr_genirating(url, foreground_color, background_color):
     image.save(filepath)
     print(f'QR-код создан: {filename}')
 
-    # Возвращаем путь для фронта
+   
     return f"/static/image/{filename}"
+
 
